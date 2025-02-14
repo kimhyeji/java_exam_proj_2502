cd2205689fe47fac55a530dfd18cac302ac7cb7d
kimhyeji <gpwl5589@gmail.com>
2025-02-14 15:48:32 +0900

    스캐너 입력 받기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("== 프로그램 시작 ==");
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("명령어) ");
+    String cmd = sc.nextLine(); // 띄어쓰기 까지 받는게 가능
+    System.out.printf("입력된 명령어 : %s\n", cmd);
+
+    sc.close();
+    System.out.println("== 프로그램 끝 ==");
   }
 }
