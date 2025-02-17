e134d3acf8c5af6a0c6bdaacacbae220ad8b1eba
kimhyeji <gpwl5589@gmail.com>
2025-02-17 15:47:08 +0900

    게시물 저장하기 전 단계, 구조만

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,35 @@ public class Main {
     System.out.println("== 프로그램 시작 ==");
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("명령어) ");
-    String cmd = sc.nextLine();
-    System.out.printf("입력된 명령어 : %s\n", cmd);
+    int lastArticleId = 0;
+
+    while ( true ) {
+      System.out.print("명령어) ");
+      String cmd = sc.nextLine();
+      cmd = cmd.trim();
+
+      if ( cmd.length() == 0 ) {
+        continue;
+      }
+
+      if ( cmd.equals("exit") ) {
+        break;
+      }
+      else if ( cmd.equals("article list") ) {
+        System.out.println("게시물이 없습니다.");
+      }
+      else if ( cmd.equals("article write") ) {
+        int id = lastArticleId + 1;
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+
+        lastArticleId = id;
+
+        System.out.printf("%d번 글이 작성되었습니다.\n", id);
+      }
+    }
 
     sc.close();
     System.out.println("== 프로그램 끝 ==");
