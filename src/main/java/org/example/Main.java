ed6228ee9d81be37f14aec6231132b684e47fd53
kimhyeji <gpwl5589@gmail.com>
2025-02-18 15:45:56 +0900

    게시물 작성 게시물 상세페이지 기능 구현, 날짜는 아직

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -37,7 +37,6 @@ public class Main {
 
           System.out.printf("%d | %s\n", article.id, article.title);
         }
-
       }
       else if ( cmd.equals("article write") ) {
         int id = lastArticleId + 1;
@@ -53,6 +52,33 @@ public class Main {
 
         System.out.printf("%d번 글이 작성되었습니다.\n", id);
       }
+      else if ( cmd.startsWith("article detail ") ) {
+        String[] cmdBits = cmd.split(" ");
+        int id = Integer.parseInt(cmdBits[2]); // "1" => 1
+
+        // article detail 1
+        Article foundArticle = null;
+
+        for ( int i = 0; i < articles.size(); i++ ) {
+          Article article = articles.get(i);
+
+          if ( article.id == id ) {
+            foundArticle = article;
+            break;
+          }
+        }
+
+        if ( foundArticle == null ) {
+          System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+          continue;
+        }
+
+        System.out.printf("번호 : %d\n", foundArticle.id);
+        System.out.printf("날짜 : %s\n", "2025-12-12 12:12:12");
+        System.out.printf("제목 : %s\n", foundArticle.title);
+        System.out.printf("내용 : %s\n", foundArticle.body);
+      }
+
     }
 
     sc.close();
