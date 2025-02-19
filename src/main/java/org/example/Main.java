10cc6a7d19d65a593d203fde2933c463ac9d8c07
kimhyeji <gpwl5589@gmail.com>
2025-02-19 15:16:43 +0900

    게시물 삭제작성 시 작성 날짜 저장

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -40,13 +40,14 @@ public class Main {
       }
       else if ( cmd.equals("article write") ) {
         int id = lastArticleId + 1;
+        lastArticleId = id;
+        String regDate = Util.getNotDateStr();
         System.out.printf("제목 : ");
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String body = sc.nextLine();
 
-        lastArticleId = id;
-        Article article = new Article(id, title, body);
+        Article article = new Article(id, regDate, title, body);
 
         articles.add(article);
 
@@ -74,7 +75,7 @@ public class Main {
         }
 
         System.out.printf("번호 : %d\n", foundArticle.id);
-        System.out.printf("날짜 : %s\n", "2025-12-12 12:12:12");
+        System.out.printf("날짜 : %s\n", foundArticle.regDate);
         System.out.printf("제목 : %s\n", foundArticle.title);
         System.out.printf("내용 : %s\n", foundArticle.body);
       }
@@ -112,11 +113,13 @@ public class Main {
 
 class Article {
   int id;
+  String regDate;
   String title;
   String body;
 
-  public Article (int id, String title, String body) {
+  public Article (int id, String regDate, String title, String body) {
     this.id = id;
+    this.regDate = regDate;
     this.title = title;
     this.body = body;
   }
